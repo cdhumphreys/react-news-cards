68165085a17cd1c37cc8e3529e41405eafb28562
Carl Humphreys <chumphreys@opera.com>
2016-07-08 12:04:17 +0100

    changed way that new cards can be made, changed structure of card content data

--- src/index.js
+++ src/index.js
@@ -4,19 +4,49 @@ import Card from './Components/Card';
 
 require('./styles/stylesheet.css');
 
-const textData = {
-  heading: 'Pretty Pictures',
-  subheading: 'What do you think?',
-  content: 'Blah blah blah, blah blah blah, some more blah blah blah'
+const textData1 = {
+  thumbnail: 'https://images.unsplash.com/photo-1464655646192-3cb2ace7a67e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=7bd13db3b64ce66833f51169e9e6e5e5',
+  colorTheme: 'orange',
+  category: 'Photos',
+  comments: 9,
+  text: {
+    heading: 'Pretty Pictures',
+    subheading: 'Magical photons taking photos',
+    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  }
+};
+const textData2 = {
+  thumbnail: 'https://images.unsplash.com/photo-1424847651672-bf20a4b0982b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=9bd244971b9cb52a8418cf2f66fce464',
+  colorTheme: 'blue',
+  category: 'Food',
+  comments: 24,
+  text: {
+    heading: 'Kitchen Nightmares',
+    subheading: 'There are rats everywhere',
+    content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  }
 };
 
-const thumbnailSrc = 'https://images.unsplash.com/photo-1464655646192-3cb2ace7a67e?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=7bd13db3b64ce66833f51169e9e6e5e5';
+const cardData = [textData1, textData2];
+
 
 class App extends React.Component {
   render() {
+    const cardNodes = cardData.map((cardData, index) => {
+     const {thumbnail, colorTheme, category, text, comments} = cardData;
+       return (
+         <Card thumbnailSrc={thumbnail} colorTheme={colorTheme} category={category} textContent={text} numComments={comments} key={index}/>
+       );
+     });
     return (
       <div>
-        <Card thumbnailSrc={thumbnailSrc} colorTheme={'orange'} category={'photos'} textContent={textData} numComments={5}/>
+        {cardNodes}
       </div>
     )
   }
