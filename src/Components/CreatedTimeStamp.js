0315a2c3f6489c33a43a186cae25a972ea23233d
Carl Humphreys <humphreys.carl.d@gmail.com>
2016-07-10 03:20:14 +0100

    fixing created timer to update and re-render when timer changes

--- src/Components/CreatedTimeStamp.js
+++ src/Components/CreatedTimeStamp.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 class CreatedTimeStamp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      minutesAgoCreated: 0
+    };
+  }
+
+
   render() {
 
     let currentTime = Date.now();
     setInterval(function () {
       currentTime = Date.now();
-    }, 2000);
-    let timeElapsed = (currentTime - this.props.timeCreated).toFixed(0);
+        let minutesElapsed = ((currentTime - this.props.timeCreated)/(1000*60)).toFixed(0);
+        this.setState({
+          minutesAgoCreated: minutesElapsed
+        });
+    }.bind(this), 1000);
+
     return (
       <div className="timeCreated">
-        <span> Created <span>{timeElapsed}</span> minutes ago </span>
+        <span> Created <span>{this.state.minutesAgoCreated}</span> minutes ago </span>
       </div>
     )
   }
